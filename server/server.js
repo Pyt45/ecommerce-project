187e71e7af73db27d7350b6f2ce1e366eb081147
Ayoub Aqlzim <aaqlzim@e2r7p1.1337.ma>
2021-09-17 18:20:59 +0100

    progress

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const connectDb = require('./utils/connectDb');
 const userRouter = require('./routes/user');
+// const productRouter = require('./routes/product');
 const app = express();
 
 
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extends: true }));
 
 app.use('/users', userRouter);
+// app.use('/products', productRouter);
 
 
 app.use('*', (req, res) => {
